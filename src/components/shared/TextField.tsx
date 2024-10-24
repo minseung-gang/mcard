0b7b2e92367fa007bfb04f12275e29013d87ce58
minseung-gang <strause1@nate.com>
2024-10-24 12:21:01 +0900

    Feat: TextField 공용 컴포넌트 추가

---
+++ src/components/shared/TextField.tsx
@@ -0,0 +1,67 @@
+import {
+  FocusEventHandler,
+  forwardRef,
+  InputHTMLAttributes,
+  useState,
+} from 'react'
+import Input from './Input'
+import Text from './Text'
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: React.ReactNode
+  hasError?: boolean
+  helpMessage?: React.ReactNode
+}
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  function TextField(
+    { label, hasError, helpMessage, onFocus, onBlur, ...props },
+    ref,
+  ) {
+    const [focused, setFocused] = useState(false)
+    //TODO: 포커스처리
+    const labelColor = hasError ? 'red' : focused ? 'blue' : undefined
+
+    const handleFocus: FocusEventHandler<HTMLInputElement> = (event) => {
+      setFocused(true)
+      onFocus?.(event)
+    }
+    const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+      setFocused(false)
+      onFocus?.(event)
+    }
+    return (
+      <div>
+        {label && (
+          <Text
+            typography="t7"
+            color={labelColor}
+            display="inline-block"
+            style={{ marginBottom: 6 }}
+          >
+            {label}
+          </Text>
+        )}
+
+        <Input
+          ref={ref}
+          aria-invalid={hasError}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...props}
+        />
+        {helpMessage && (
+          <Text
+            typography="t7"
+            display="inline-block"
+            style={{ marginTop: 6, fontSize: 12 }}
+          >
+            {helpMessage}
+          </Text>
+        )}
+      </div>
+    )
+  },
+)
+
+export default TextField
