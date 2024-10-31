13af1d401a37ea168acf1f5b619782534f4afaca
minseung-gang <strause1@nate.com>
2024-10-31 21:35:26 +0900

    Feat: 카드 세부 페이지 신청하기 버튼 추가

---
+++ src/components/shared/FixedBottomButton.tsx
@@ -0,0 +1,50 @@
+import { colors } from '@/styles/colorPalette'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import React from 'react'
+import { createPortal } from 'react-dom'
+import Button from './Button'
+
+interface FixedBottomButtonProps {
+  label: string
+  onClick: () => void
+  disabled?: boolean
+}
+
+export default function FixedBottomButton({
+  label,
+  onClick,
+  disabled,
+}: FixedBottomButtonProps) {
+  const $portalRoot = document.getElementById('root-portal')
+
+  if ($portalRoot == null) return null
+
+  return createPortal(
+    <Container>
+      <Button
+        size="medium"
+        disabled={disabled}
+        full={true}
+        onClick={onClick}
+        css={buttonStyles}
+      >
+        {label}
+      </Button>
+    </Container>,
+    $portalRoot,
+  )
+}
+
+const Container = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${colors.white};
+  padding: 20px 10px 8px;
+`
+
+const buttonStyles = css`
+  border-radius: 8px;
+`
