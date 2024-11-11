483740a98d18d7990f4d49b077cc14d3eee1e543
minseung-gang <strause1@nate.com>
2024-11-11 22:44:01 +0900

    Add: route 경로  최신화 및 scrollTop 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Card from './pages/Card'
-import HomePage from './pages/Home'
-import TestPage from './pages/Test'
+import ScrollToTop from '@/components/shared/ScrollToTop'
+import Card from '@/pages/Card'
+import HomePage from '@/pages/Home'
+import TestPage from '@/pages/Test'
+import SigninPage from './pages/Signin'
+import SignupPage from './pages/Signup'
+import Navbar from './components/shared/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/test" Component={TestPage} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
         <Route path="/card/:id" Component={Card} />
       </Routes>
     </BrowserRouter>
