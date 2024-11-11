8e60d67b9a83458472b2fe3452de611366bf5272
minseung-gang <strause1@nate.com>
2024-11-11 22:42:57 +0900

    Feat: Nav 컴포넌트 추가

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,33 @@
+import { colors } from '@/styles/colorPalette'
+import { css } from '@emotion/react'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import Button from './Button'
+import Flex from './Flex'
+
+export default function Navbar() {
+  const location = useLocation()
+  const showSignButton =
+    ['/signup', '/signin'].includes(location.pathname) === false
+  return (
+    <Flex justify="space-between" align="center" css={navbarContainerStyles}>
+      <Link to="/">홈</Link>
+      {showSignButton && (
+        <Link to="/signup">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )}
+    </Flex>
+  )
+}
+
+const navbarContainerStyles = css`
+  height: 55px;
+  padding: 10px 24px;
+  box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid ${colors.grey};
+`
