6359dc25f260a19fb6347e4a7032c6ad256a8dad
minseung-gang <strause1@nate.com>
2024-11-12 22:07:05 +0900

    Feat: 회원가입 페이지 추가

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,34 @@
 import Form from '@/components/signup/Form'
-import React from 'react'
+import { COLLECTIONS } from '@/constants'
+import { FormValues } from '@/models/signup'
+import { auth, store } from '@/remote/firebase'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
 
 export default function SignupPage() {
+  const navigate = useNavigate()
+  const handleSubmit = async (formValues: FormValues) => {
+    const { email, password, name } = formValues
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+
+    await updateProfile(user, {
+      displayName: name,
+    })
+
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: name,
+    }
+
+    await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser)
+
+    navigate('/')
+  }
   return (
     <div>
-      <Form />
+      <Form onSubmit={handleSubmit} />
     </div>
   )
 }
