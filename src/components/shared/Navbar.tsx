dcd4f80512b6b10358fd84cfdcd62cdf0f44ba19
minseung-gang <strause1@nate.com>
2024-11-13 22:53:04 +0900

    Refector: 로그인 유무를 파악하여 로그인/로그아웃 버튼 구현하도록 수정

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,22 +1,40 @@
+import useUser from '@/hooks/auth/useUser'
+import { auth } from '@/remote/firebase'
 import { colors } from '@/styles/colorPalette'
 import { css } from '@emotion/react'
-import React from 'react'
+import { signOut } from 'firebase/auth'
+import { useCallback } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import Button from './Button'
 import Flex from './Flex'
 
 export default function Navbar() {
   const location = useLocation()
+  const user = useUser()
+
   const showSignButton =
     ['/signup', '/signin'].includes(location.pathname) === false
-  return (
-    <Flex justify="space-between" align="center" css={navbarContainerStyles}>
-      <Link to="/">홈</Link>
-      {showSignButton && (
+
+  const handleLogout = useCallback(() => {
+    signOut(auth)
+  }, [])
+  const renderButton = useCallback(() => {
+    if (user !== null) {
+      return <Button onClick={handleLogout}>로그아웃</Button>
+    }
+    if (showSignButton) {
+      return (
         <Link to="/signin">
           <Button>로그인/회원가입</Button>
         </Link>
-      )}
+      )
+    }
+  }, [user, showSignButton])
+
+  return (
+    <Flex justify="space-between" align="center" css={navbarContainerStyles}>
+      <Link to="/">홈</Link>
+      {renderButton()}
     </Flex>
   )
 }
