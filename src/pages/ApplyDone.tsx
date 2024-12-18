e1b9ea3f84007fd35c553613bed404026edc2ec7
minseung-gang <strause1@nate.com>
2024-12-18 16:05:03 +0900

    Feat: 카드 신청 완료 후 페이지 추가

---
+++ src/pages/ApplyDone.tsx
@@ -0,0 +1,20 @@
+import FixedBottomButton from '@/components/shared/FixedBottomButton'
+import Flex from '@/components/shared/Flex'
+import Text from '@/components/shared/Text'
+import { parse } from 'qs'
+import React from 'react'
+
+export default function ApplyDone() {
+  const { success } = parse(window.location.search, { ignoreQueryPrefix: true })
+
+  return (
+    <Flex>
+      <Text>
+        {success === 'true'
+          ? '카드가 발급되었습니다.'
+          : '카드 발급에 실패했습니다'}
+      </Text>
+      <FixedBottomButton label="확인" onClick={() => window.history.back()} />
+    </Flex>
+  )
+}
